21ff3599f9142bb9df3a4af005c53a9c2b4c2e33
ZebbyOgada <zebbyoloo@gmail.com>
2023-06-16 15:29:27 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
-#includ<stdio.h>
-int main()
+#include <stdio.h>
+/**
+ * this program returns a zero for succes
+ * and otherwise
+ */
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
