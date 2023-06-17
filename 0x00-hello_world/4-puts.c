32b3863a092ed7b18193e1fac659a0783a47e510
ZebbyOgada <zebbyoloo@gmail.com>
2023-06-17 11:48:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * this program returns a zero for succes
  * and otherwise
  */
-int main (void)
+int main(void)
 {
 /*the value returns  a zero for a success*/
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return(0);
 }
